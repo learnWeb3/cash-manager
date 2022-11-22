332fa385430af3624167af45e3e1d8664d280871
= <=>
2022-11-22 10:54:36 +0100

    update code

---
+++ bank/src/errors/index.ts
@@ -0,0 +1,50 @@
+export class InternalServerError extends Error {
+  statusCode: number;
+  statusMessage: string;
+  constructor(
+    message,
+    statusCode = 500,
+    statusMessage = "Internal Server Error"
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.statusMessage = statusMessage;
+  }
+}
+
+export class BadRequestError extends Error {
+  statusCode: number;
+  statusMessage: string;
+  constructor(message, statusCode = 400, statusMessage = "Bad Request") {
+    super(message);
+    this.statusCode = statusCode;
+    this.statusMessage = statusMessage;
+  }
+}
+
+export class ForbiddenError extends Error {
+  statusCode: number;
+  statusMessage: string;
+  constructor(message, statusCode = 403, statusMessage = "Forbidden") {
+    super(message);
+    this.statusCode = statusCode;
+    this.statusMessage = statusMessage;
+  }
+}
+
+export class UnauthorizedError extends Error {
+  statusCode: number;
+  statusMessage: string;
+  constructor(message, statusCode = 401, statusMessage = "Unauthorized") {
+    super(message);
+    this.statusCode = statusCode;
+    this.statusMessage = statusMessage;
+  }
+}
+
+export class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+  }
+}
+
