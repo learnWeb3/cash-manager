25ad74ef1adb97deb32cc9c130190cb2f7bb6a22
Alexis Massard <alexis@nstechno.com>
2022-12-06 10:35:59 +0100

    dashboard

--- server/src/services/env.service.ts
+++ server/src/services/env.service.ts
@@ -27,9 +27,9 @@ const environment: Environment = {
     PORT: parseInt(process.env.PORT) as Environment["PORT"],
     MONGO_HOST: process.env.MONGO_HOST as Environment["MONGO_HOST"],
     CLIENT_URL: process.env.CLIENT_URL as Environment["CLIENT_URL"],
-    MONGO_USERNAME: process.env.MONGO_USERNAME as Environment["MONGO_USERNAME"],
-    MONGO_PASSWORD: process.env.MONGO_PASSWORD as Environment["MONGO_PASSWORD"],
-    MONGO_DATABASE: process.env.MONGO_DATABASE as Environment["MONGO_DATABASE"],
+    MONGO_USERNAME: process.env.MONGO_INITDB_ROOT_USERNAME as Environment["MONGO_USERNAME"],
+    MONGO_PASSWORD: process.env.MONGO_INITDB_ROOT_PASSWORD as Environment["MONGO_PASSWORD"],
+    MONGO_DATABASE: process.env.MONGO_INITDB_DATABASE as Environment["MONGO_DATABASE"],
     // MONGO_REPLICA_NAME: process.env.MONGO_REPLICA_NAME as Environment["MONGO_REPLICA_NAME"],
     SESSION_EXPIRE: process.env.SESSION_EXPIRE as Environment["SESSION_EXPIRE"],
     SESSION_SECRET: process.env.SESSION_SECRET as Environment["SESSION_SECRET"]
