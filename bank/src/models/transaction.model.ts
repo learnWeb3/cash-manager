ee49ce28680e812d08c30de4c3fa09c0431228ed
= <=>
2023-01-10 07:30:51 +0100

    update code moving from crypto node library to tweetnacl easier to implement on expo react native

--- bank/src/models/transaction.model.ts
+++ bank/src/models/transaction.model.ts
@@ -1,94 +1,104 @@
 import { model, Schema, Model, HydratedDocument, ObjectId } from "mongoose";
-import { transactionStatus } from './transaction-status';
-import { createVerify } from 'node:crypto';
+import { transactionStatus } from "./transaction-status";
 import { Account } from "./account.model";
-import { readFile, readFileSync } from "node:fs";
-import { join } from 'node:path';
+import nacl from "tweetnacl";
+import util from "tweetnacl-util";
 
-const { Number, ObjectId } = Schema.Types
+//@ts-ignore
+nacl.util = util;
+
+const { Number, ObjectId } = Schema.Types;
 
 export interface ITransaction {
-    from: ObjectId,
-    to: ObjectId,
-    transactionStatus: number,
-    amount: number,
-    signature: string
+  from: ObjectId;
+  to: ObjectId;
+  transactionStatus: number;
+  amount: number;
+  signature: string;
 }
 
 export interface ITransactionMethods {
-    verify: () => Promise<boolean>
-    verifyTx: (data: string, signature: string, publicKey:string) => boolean
+  verify: () => Promise<boolean>;
+  verifyTx: (data: string, signature: string, publicKey: string) => boolean;
 }
 
-export interface TransactionModel extends Model<ITransaction, {}, ITransactionMethods> {
-    register: (data: {
-        from: string,
-        to: string,
-        amount: number,
-        signature: string
-    }) => Promise<HydratedDocument<ITransaction, {}, ITransactionMethods>>
+export interface TransactionModel
+  extends Model<ITransaction, {}, ITransactionMethods> {
+  register: (data: {
+    from: string;
+    to: string;
+    amount: number;
+    signature: string;
+  }) => Promise<HydratedDocument<ITransaction, {}, ITransactionMethods>>;
 }
 
-const transactionSchema = new Schema<ITransaction, TransactionModel, ITransactionMethods>({
+const transactionSchema = new Schema<
+  ITransaction,
+  TransactionModel,
+  ITransactionMethods
+>(
+  {
     from: {
-        type: ObjectId,
-        ref: "Account",
-        required: true,
+      type: ObjectId,
+      ref: "Account",
+      required: true,
     },
     to: {
-        type: ObjectId,
-        ref: "Account",
-        required: true,
+      type: ObjectId,
+      ref: "Account",
+      required: true,
     },
     signature: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     transactionStatus: {
-        type: Number,
-        default: transactionStatus.PENDING,
-        required: true,
+      type: Number,
+      default: transactionStatus.PENDING,
+      required: true,
     },
     amount: {
-        type: Number,
-        default: 0,
-        required: true
-    }
-}, {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+  },
+  {
     toJSON: { virtuals: true },
     toObject: { getters: true, virtuals: true },
     timestamps: true,
-})
+  }
+);
 
-transactionSchema.method('verify', async function () {
-    const populatedTransaction = await this.populate({ path: 'from to' })
-    const { from, to, amount, signature } = populatedTransaction
-    const data = JSON.stringify({
-        from: from.id,
-        to: to.id,
-        amount
-    })
-    const check = this.verifyTx(data, signature, from.key)
+transactionSchema.method("verify", async function () {
+  const populatedTransaction = await this.populate({ path: "from to" });
+  const { from, to, amount, signature } = populatedTransaction;
+  const data = JSON.stringify({
+    from: from.id,
+    to: to.id,
+    amount,
+  });
+  const check = this.verifyTx(data, signature, from.key);
+  return check;
+});
+
+transactionSchema.method(
+  "verifyTx",
+  function (data: string, base64Signature: string, base64PublicKey: string) {
+    //@ts-ignore
+    const message = nacl.util.decodeUTF8(data);
+    //@ts-ignore
+    const publicKey = nacl.util.decodeBase64(base64PublicKey);
+    //@ts-ignore
+    const signature = nacl.util.decodeBase64(base64Signature);
+    const check = nacl.sign.detached.verify(message, signature, publicKey);
     return check;
-})
+  }
+);
 
-transactionSchema.method('verifyTx', function (data: string, signature: string, publicKey:string) {
-    const bufferedData = Buffer.from(data);
-    const verify = createVerify('SHA256');
-    verify.write(bufferedData);
-    verify.end();
-    const check = verify.verify(publicKey, signature, 'base64');
-    console.log(check)
-    return check
-})
-
-transactionSchema.static('register', async function (data: {
-    from: '',
-    to: '',
-    signature: '',
-    amount: 0
-}) {
-
+transactionSchema.static(
+  "register",
+  async function (data: { from: ""; to: ""; signature: ""; amount: 0 }) {
     const errors = [];
     const { from, to, signature, amount } = data;
 
@@ -96,22 +106,24 @@ transactionSchema.static('register', async function (data: {
     const toAccount = await Account.findById(to);
 
     if (!fromAccount) {
-        errors.push(`sending account does not exists using id ${from}`)
+      errors.push(`sending account does not exists using id ${from}`);
     }
 
     if (!toAccount) {
-        errors.push(`recipient account does not exists using id ${to}`)
+      errors.push(`recipient account does not exists using id ${to}`);
     }
 
     if (errors.length) {
-        throw new Error(errors.join(`Invalid accounts specified, reasons: ${errors.join(',')}`))
+      throw new Error(
+        errors.join(`Invalid accounts specified, reasons: ${errors.join(",")}`)
+      );
     }
 
     const newTransaction = new this({
-        from,
-        to,
-        amount,
-        signature
+      from,
+      to,
+      amount,
+      signature,
     });
 
     const registeredTransaction = await newTransaction.save();
@@ -119,25 +131,31 @@ transactionSchema.static('register', async function (data: {
     const checkSignature = await registeredTransaction.verify();
 
     if (!checkSignature) {
-        registeredTransaction.transactionStatus = transactionStatus.INVALID_SIGNATURE_ERROR
-        return await registeredTransaction.save()
+      registeredTransaction.transactionStatus =
+        transactionStatus.INVALID_SIGNATURE_ERROR;
+      return await registeredTransaction.save();
     }
 
-    const checkBalance = fromAccount.balance - amount >= 0
+    const checkBalance = fromAccount.balance - amount >= 0;
 
     if (!checkBalance) {
-        registeredTransaction.transactionStatus = transactionStatus.INSUFFICIENT_BALANCE_ERROR
-        return await registeredTransaction.save()
+      registeredTransaction.transactionStatus =
+        transactionStatus.INSUFFICIENT_BALANCE_ERROR;
+      return await registeredTransaction.save();
     }
 
     fromAccount.balance = fromAccount.balance - registeredTransaction.amount;
-    toAccount.balance = toAccount.balance + registeredTransaction.amount
+    toAccount.balance = toAccount.balance + registeredTransaction.amount;
 
     await fromAccount.save();
     await toAccount.save();
 
-    registeredTransaction.transactionStatus = transactionStatus.APPROVED
-    return await registeredTransaction.save()
-})
+    registeredTransaction.transactionStatus = transactionStatus.APPROVED;
+    return await registeredTransaction.save();
+  }
+);
 
-export const Transaction = model<ITransaction, TransactionModel>('Transaction', transactionSchema)
+export const Transaction = model<ITransaction, TransactionModel>(
+  "Transaction",
+  transactionSchema
+);
--- bank/src/scripts/gen-rsa.ts
+++ bank/src/scripts/gen-rsa.ts
@@ -1,53 +1,35 @@
-import { createVerify, generateKeyPair, sign, verify } from 'node:crypto';
-import { writeFileSync } from 'node:fs';
-import { join } from 'node:path';
+import nacl from "tweetnacl";
+import util from "tweetnacl-util";
+import { writeFileSync } from "node:fs";
+import { join } from "node:path";
 
-generateKeyPair('rsa', {
-    modulusLength: 512,
-    publicKeyEncoding: {
-        type: 'spki',
-        format: 'pem'
-    },
-    privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'pem',
-        cipher: 'aes-256-cbc',
-        passphrase: 'test'
-    }
-}, (err, publicKey, privateKey) => {
-    console.log('Testing key pair with a sample message\n\n')
-
-    const smapleMessage = JSON.stringify({
-        from: 1,
-        to: 2,
-        amount: 100
-    });
-    // Convert string to buffer 
-    const data = Buffer.from(smapleMessage);
-
-    // Sign the data and returned signature in buffer 
-    const signature = sign("SHA256", data, {
-        key: privateKey,
-        passphrase: 'test'
-    });
-
-    // Convert returned buffer to base64
-    const base64Signature = signature.toString('base64');
-
-
-    const verify = createVerify('SHA256');
-    verify.write(data);
-    verify.end();
-    const check = verify.verify(publicKey, base64Signature, 'base64');
-
-    console.log(`Signature check using a sample message output ${check}\n\n`)
-    console.log(`Signature:\n\n ${base64Signature}`);
-    console.log(`Sample message: ${JSON.stringify(smapleMessage, null, 4)}\n\n`)
-    console.log(`Please find your public/private key pair below:\n\n`)
-    console.log(`Public key is:\n\n ${publicKey}`)
-    console.log(`Private key is:\n\n ${privateKey}`)
-
-    writeFileSync(join(process.cwd(), 'pk.txt'), privateKey, { encoding: 'utf-8' });
-    writeFileSync(join(process.cwd(), 'pk.pub.txt'), publicKey, { encoding: 'utf-8' })
-    process.exit(0);
+//@ts-ignore
+nacl.util = util;
+const { publicKey, secretKey } = nacl.sign.keyPair();
+const sampleMessage = JSON.stringify({
+  from: 1,
+  to: 2,
+  amount: 100,
 });
+//@ts-ignore
+const messageUINT8Array = nacl.util.decodeUTF8(sampleMessage);
+//@ts-ignore
+nacl.util.encodeBase64(nacl.sign(messageUINT8Array, secretKey));
+
+writeFileSync(
+  join(process.cwd(), "pk.txt"),
+  //@ts-ignore
+  nacl.util.encodeBase64(secretKey),
+  {
+    encoding: "utf-8",
+  }
+);
+writeFileSync(
+  join(process.cwd(), "pk.pub.txt"),
+  //@ts-ignore
+  nacl.util.encodeBase64(publicKey),
+  {
+    encoding: "utf-8",
+  }
+);
+process.exit(0);
