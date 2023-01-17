acece60e62a68dc2c938efbbc8c024885c1742a1
= <=>
2023-01-17 13:30:55 +0100

    update code

--- CashApp/components/NFCManager/index.js
+++ CashApp/components/NFCManager/index.js
@@ -1,12 +1,16 @@
 import * as React from "react";
 import NfcManager, { Ndef, NfcEvents } from "react-native-nfc-manager";
 
+NfcManager.start();
+
 export const NFCManager = ({
   inpuRef = null,
   children = null,
   setTag = () => {},
 }) => {
   React.useEffect(() => {
+
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
     NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
       const message = Ndef.text.decodePayload(tag.ndefMessage[0].payload);
       const messageWithoutLanguageCode = message.replace(/^en(.*)$/, "$1");
